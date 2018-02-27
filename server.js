fd01703d44a3da5506e2f3682f3c3ad90b81635a
firsti <brehmedu@fim.uni-passau.de>
2018-02-27 14:44:04 +0100

    Retry to bring agile-otp up, until the client is added. Update all entities seen, when searching for token upon query

--- server.js
+++ server.js
@@ -15,6 +15,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 //Authenticate on boot
 agile.idm.authentication.authenticateClient(conf.client.id, conf.client.clientSecret).then((auth) => {
   agile.tokenSet(auth.access_token);
+  app.use(compression());
+  // All gateway traffic proxied with signature <host>/<service-name>
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  app.use(bearerToken());
+  app.use('/agile-security', authMiddleware, proxyFactory('agile-security', 'agile-security', 3000));
+  app.use('/agile-core', authMiddleware, proxyFactory('agile-core', 'agile-core', 8080, true));
+  app.use('/agile-data', authMiddleware, proxyFactory('agile-data', 'agile-data', 1338));
+  app.use('/agile-recommender', authMiddleware, proxyFactory('agile-recommender', 'agile-recommender', 1338));
+
+  app.listen(conf.port, () => console.log('AGILE-OTP proxy listening on', conf.port));
 }).catch(err => {
   console.log(err)
 }).then(() => {
@@ -22,18 +33,10 @@ agile.idm.authentication.authenticateClient(conf.client.id, conf.client.clientSe
   getEntities(conf.types).then(entities => {
     if (entities) {
       entities.forEach(entity => {
-        if (entity.credentials && entity.credentials.otp && !entity.credentials.otp.frame) {
-          let frame = {};
-          for (let i = 1; i <= conf.frame_size; ++i) {
-            frame[i] = otp.generateEID(entity.credentials.otp.ik, 0, parseInt(entity.credentials.otp.ts) + i - 1).eid
-          }
+        if (entity.credentials && entity.credentials.otp && entity.credentials.otp.ik && entity.credentials.otp.ts && !entity.credentials.otp.frame) {
+          let frame = getFrame(entity.credentials.otp.ik, parseInt(entity.credentials.otp.ts));
           entity.credentials.otp.frame = frame;
-          agile.idm.entity.setAttribute({
-            entityType: entity.type.replace("/", ""),
-            entityId: entity.id,
-            attributeType: 'credentials.otp',
-            attributeValue: entity.credentials.otp
-          }).catch(err => {
+          updateEntity(entity).catch(err => {
             console.log(err);
           }).then(entity => {
             console.log("Created token for", entity.id);
@@ -64,6 +67,26 @@ function getEntities(types) {
   });
 }
 
+const getFrame = function (ik, ts) {
+  let frame = {};
+  for (let i = 0; i < conf.frame_size; ++i) {
+    frame[i] = otp.generateEID(ik, 0, ts + i).eid;
+  }
+  return frame;
+}
+
+const updateEntity = function (entity) {
+  return agile.idm.entity.setAttribute({
+    entityType: entity.type.replace("/", ""),
+    entityId: entity.id,
+    attributeType: 'credentials.otp',
+    attributeValue: entity.credentials.otp
+  }).catch(err => {
+    console.log("Could not set new frame for entity");
+    console.log(err.statusCode, err.message);
+  })
+}
+
 const authMiddleware = function (req, res, next) {
   const token = tokenParser(req);
   if (!token) {
@@ -73,41 +96,34 @@ const authMiddleware = function (req, res, next) {
 
   getEntities(conf.types).then(entities => {
     return entities.filter(entity => {
-      if (entity.credentials && entity.credentials.otp && entity.credentials.otp.frame) {
-        let keys = Object.keys(entity.credentials.otp.frame);
+      if (entity.credentials && entity.credentials.otp && entity.credentials.otp.ik && entity.credentials.otp.ts) {
+        let frame = getFrame(entity.credentials.otp.ik, parseInt(entity.credentials.otp.ts));
+        entity.credentials.otp.frame = frame;
+        let keys = Object.keys(frame);
         let foundKey = keys.filter(key => {
           return entity.credentials.otp.frame[key] === token
         })[0];
         if (foundKey) {
-          entity.credentials.otp.ts = parseInt(entity.credentials.otp.ts) + parseInt(foundKey[0]);
-          return entity
+          entity.credentials.otp.ts = parseInt(entity.credentials.otp.ts) + parseInt(foundKey[0]) + 1;
+          entity.credentials.otp.frame = getFrame(entity.credentials.otp.ik, parseInt(entity.credentials.otp.ts));
+          return updateEntity(entity)
+        } else {
+          updateEntity(entity)
         }
       }
     })[0];
   }).then(entity => {
     if (entity) {
-      let frame = {};
-      for (let i = 1; i <= conf.frame_size; ++i) {
-        frame[i] = otp.generateEID(entity.credentials.otp.ik, 0, entity.credentials.otp.ts + i - 1).eid
-      }
-      entity.credentials.otp.frame = frame;
-      agile.idm.entity.setAttribute({
-        entityType: entity.type.replace("/", ""),
-        entityId: entity.id,
-        attributeType: 'credentials.otp',
-        attributeValue: entity.credentials.otp
-      }).then(() => {
-        //If everything is OK, add the clients' token to be able to proxy the request
-        req.headers = {'authorization': 'Bearer ' + agile.tokenGet(), 'Content-Type': 'application/json'};
-        next();
-      });
+      //If everything is OK, add the clients' token to be able to proxy the request
+      req.headers = {'authorization': 'Bearer ' + agile.tokenGet(), 'Content-Type': 'application/json'};
+      next();
     } else {
-      throw {response: {status: 401}, message: 'Invalid token'}
+      throw {status: 401, message: 'Invalid token'}
     }
   }).catch(err => {
     // forward the failed result to client
     if (err) {
-      res.status(err.response.status).send(err.message)
+      res.status(err.status).send(err.message)
     } else {
       next(err)
     }
@@ -124,15 +140,3 @@ const proxyFactory = (name, service, port, ws = false) => {
     ws
   })
 }
-
-app.use(compression());
-// All gateway traffic proxied with signature <host>/<service-name>
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.use(bearerToken());
-app.use('/agile-security', authMiddleware, proxyFactory('agile-security', 'agile-security', 3000));
-app.use('/agile-core', authMiddleware, proxyFactory('agile-core', 'agile-core', 8080, true));
-app.use('/agile-data', authMiddleware, proxyFactory('agile-data', 'agile-data', 1338));
-app.use('/agile-recommender', authMiddleware, proxyFactory('agile-recommender', 'agile-recommender', 1338));
-
-app.listen(conf.port, () => console.log('AGILE-OTP proxy listening on', conf.port));
